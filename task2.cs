b1fe9b9393cd8376975ef931451e91dfd1213887
Nastya Naumova <nastia.nnn005@gmail.com>
2023-12-25 14:22:29 +0300

    first commit

---
+++ task2.cs
@@ -0,0 +1,6 @@
+namespace lab2;
+
+public class task2
+{
+    
+}
---
+++ task3.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace lab2
+{
+    public class DocumentWorker
+    {
+        public void OpenDocument()
+        {
+            Console.WriteLine($"Документ открыт");
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine($"Редактирование документа доступно в версии Pro");
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine($"Сохранение документа доступно в версии Pro");
+        }
+    }
+
+    public class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine($"Документ отредактирован");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine($"Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Expert");
+        }
+    }
+
+    public class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine($"Документ сохранен в новом формате");
+        }
+    }
+    internal class Program
+    { 
+        public static void Main(string[] args) 
+        {
+            string keyPro = "1234Pro"; 
+            string keyExp = "1234Exp";
+            
+            Console.WriteLine($"Please enter the key"); 
+            string userKey = Console.ReadLine();
+            if (userKey == keyPro)
+            {
+                ProDocumentWorker document1 = new ProDocumentWorker();
+                document1.OpenDocument(); 
+                document1.EditDocument();
+                document1.SaveDocument();
+            }
+            else if (userKey == keyExp)
+            {
+                    ExpertDocumentWorker document1 = new ExpertDocumentWorker();
+                    document1.OpenDocument();
+                    document1.EditDocument();
+                    document1.SaveDocument();
+            }
+            else if ((userKey != keyExp) && userKey != keyPro)
+            {
+                    DocumentWorker document1 = new DocumentWorker();
+                    document1.OpenDocument();
+                    document1.EditDocument();
+                    document1.SaveDocument();
+            }
+        }
+    }
+}
